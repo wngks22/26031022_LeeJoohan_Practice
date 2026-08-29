e7a06fabaf8ba0fc467705207cd7077614acc7ff
wngks22 <joohan1822@gmail.com>
2026-08-29 23:46:19 +0900

    작업물 저장

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Practice1
+﻿
+namespace Practice1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(10, 2);
+            Console.WriteLine("나의 이력서");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(10, 4);
+            Console.Write("이름:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("이주한");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.SetCursorPosition(9, 5);
+            Console.WriteLine("sdff");
+
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
