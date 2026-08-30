6865a292b89f1fcabe442672838b15f700353465
wngks22 <joohan1822@gmail.com>
2026-08-30 20:06:59 +0900

    자기이력서 수정

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -5,19 +5,118 @@ namespace Practice1
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.SetCursorPosition(10, 2);
-            Console.WriteLine("나의 이력서");
+            // 제목
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.SetCursorPosition(0, 0);
+            Console.Write("============================================================");
 
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.SetCursorPosition(10, 4);
-            Console.Write("이름:");
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("이주한");
+            Console.SetCursorPosition(23, 2);
+            Console.Write("[ 자기이력서 ]");
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.SetCursorPosition(0, 4);
+            Console.Write("============================================================");
+
+
+            // 기본 정보
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.SetCursorPosition(6, 6);
+            Console.Write("[ 이름 ]");
 
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.SetCursorPosition(9, 5);
-            Console.WriteLine("sdff");
+            Console.SetCursorPosition(26, 6);
+            Console.Write("[ 나이 ]");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.SetCursorPosition(46, 6);
+            Console.Write("[ 학번 ]");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(7, 7);
+            Console.Write("이주한");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(28, 7);
+            Console.Write("20세");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(46, 7);
+            Console.Write("26031022");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.SetCursorPosition(26, 9);
+            Console.Write("[ MBTI ]");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(28, 10);
+            Console.Write("INTJ");
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.SetCursorPosition(0, 12);
+            Console.Write("------------------------------------------------------------");
+
+            // 호불호, 장단점
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.SetCursorPosition(8, 14);
+            Console.Write("[ 좋아하는것 ]");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(38, 14);
+            Console.Write("[ 싫어하는것 ]");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(13, 15);
+            Console.Write("게임");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(43, 15);
+            Console.Write("국어");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.SetCursorPosition(11, 17);
+            Console.Write("[ 장점 ]");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(41, 17);
+            Console.Write("[ 단점 ]");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(11, 18);
+            Console.Write("완벽주의");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(41, 18);
+            Console.Write("완벽주의");
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.SetCursorPosition(0, 20);
+            Console.Write("------------------------------------------------------------");
+
+            // 하고 싶은 말
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(22, 22);
+            Console.Write("[ 하고 싶은 말 ]");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(14, 23);
+            Console.Write("다들 1학기동안 수고 많으셨습니다!");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(14, 25);
+            Console.Write("이번 2학기도 힘내서 좋은 분위기로");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(15, 26);
+            Console.Write("끝까지 함께 달려보고 싶습니다.");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(22, 27);
+            Console.Write("잘 부탁드립니다!");
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.SetCursorPosition(0, 29);
+            Console.Write("============================================================");
 
             Console.ForegroundColor = ConsoleColor.White;
         }
