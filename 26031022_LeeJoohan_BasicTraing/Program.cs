f40789730903cdc7c9363de5c63675087a626bd4
wngks22 <joohan1822@gmail.com>
2026-09-02 13:53:50 +0900

    변수 연습

---
+++ 26031022_LeeJoohan_BasicTraing/Program.cs
@@ -0,0 +1,33 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        ////키워드, 식별자 연습
+        //string name = "이주한";
+        //string id = "26031022";
+        //string subject = "객체지향C#";
+        //Console.WriteLine($"이름:{name}, id:{id}, 과목:{subject}");
+
+        ////연산 연습
+        //Console.WriteLine(10 + 2000);
+        //Console.WriteLine(10 + "2000");
+        //Console.WriteLine(1234 / 100);
+        //Console.WriteLine(1234 % 100);
+
+        //문자형
+        //Console.WriteLine('가');
+        //Console.WriteLine("\"안녕하세요\"\n반갑습니다.");
+        //Console.WriteLine("안"+'녕'+'하'+'세'+'요');
+        //Console.WriteLine("가"+'\uAC00');
+
+        //변수
+        int idNumber = 26031022;
+        long gameMoney = 1000000000000000000;
+        double score = 99.9;
+        char character = 'a';
+        string message = "안녕하세요";
+        var variable = 123;
+        int? Null = null;
+    }
+}
